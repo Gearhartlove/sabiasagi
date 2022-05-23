c9a69550ca85906e669c4008292aaf21576e1ee8
Kristoff <finley@kristoff.codes>
2022-05-23 20:08:20 +0200

    Text rending to screen . . . remember to add a ui camera next time. . .

--- src/constants.rs
+++ src/constants.rs
@@ -3,11 +3,13 @@ use bevy::prelude::Transform;
 pub const WINDOW_WIDTH: f32 = 512.;
 pub const WINDOW_HEIGHT: f32 = 384.;
 pub const SPRITE_SIZE: f32 = 32. * 5.;
+pub const FONT_PATH: &str = "fonts/JetBrainsMono-SemiBold.ttf";
+pub const TEXT_OFFSET: f32 = 20.;
 
-const arbitrary_y_val: f32 = 100.;
-const arbitrary_x_val: f32 = 24.;
+const ARBITRARY_Y_VAL: f32 = 100.;
+const ARBITRARY_X_VAL: f32 = 24.;
 
 pub const RIGHT_FIGHTER_TRANSFORM: Transform =
-    Transform::from_xyz((WINDOW_WIDTH/2. * -1. + SPRITE_SIZE/2. + arbitrary_x_val), (WINDOW_HEIGHT/2. * -1. + SPRITE_SIZE/2. + arbitrary_y_val), 1.);
+    Transform::from_xyz((WINDOW_WIDTH/2. * -1. + SPRITE_SIZE/2. + ARBITRARY_X_VAL), (WINDOW_HEIGHT/2. * -1. + SPRITE_SIZE/2. + ARBITRARY_Y_VAL), 1.);
 pub const LEFT_FIGHTER_TRANSFORM: Transform =
-    Transform::from_xyz((WINDOW_WIDTH/2. - SPRITE_SIZE/2. - arbitrary_x_val) , (WINDOW_HEIGHT/2. - SPRITE_SIZE/2.), 1.);
+    Transform::from_xyz((WINDOW_WIDTH/2. - SPRITE_SIZE/2. - ARBITRARY_X_VAL), (WINDOW_HEIGHT/2. - SPRITE_SIZE/2.), 1.);
--- src/main.rs
+++ src/main.rs
@@ -16,12 +16,14 @@ fn main() {
             height: WINDOW_HEIGHT,
             ..default()
         })
-        .insert_resource(ClearColor(Color::rgb(1., 1., 1.)))
+        // .insert_resource(ClearColor(Color::rgb(1., 1., 1.)))
         .add_plugins(DefaultPlugins)
         .add_startup_system_to_stage(PreStartup, setup_assets)
         .add_startup_system(setup_arena)
         .add_startup_system(camera_setup)
+        .add_startup_system(ui_camera_setup)
         // .add_system(debug_fighters)
+        .add_startup_system(debug_UI)
         .run();
 }
 
@@ -29,9 +31,19 @@ fn camera_setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
+fn ui_camera_setup(mut commands: Commands) {
+    commands.spawn_bundle(UiCameraBundle::default());
+}
+
 fn debug_fighters(query: Query<&Fighter>) {
     for fighter in query.iter() {
         println!("{:?}", fighter);
+    }
+}
+
+fn debug_UI(query: Query<&Text>) {
+    for text in query.iter() {
+         println!("{:?}", text);
     }
 }
 
@@ -79,5 +91,34 @@ fn setup_arena(mut commands: Commands, asset_server: Res<AssetServer>) {
         .insert(charmeleon);
 
     // CREATE HEALTH, NAME, LEVEL -------------------------------------------------------
-    commands.spawn()
+    // Note, I will not follow the picture exactly, I will have an above and
+    // below variant to put the text into
+    // todo: above or below variant
+    // todo: think about how I want to connect all of the UI elements to the fighters
+    //  could append the fighter to each tag, or make a p1 and p2 component and append it likewise,
+    //  or ???
+    // NAME
+    // left text
+    println!("spawning_text");
+    commands.spawn_bundle(TextBundle {
+        style: Style {
+            align_self: AlignSelf::FlexEnd,
+            position_type: PositionType::Absolute,
+            margin: Rect::all(Val::Px(10.0)),
+            ..default()
+            },
+        text: Text::with_section(
+            "pokemon_name",
+            TextStyle {
+                font: asset_server.load(FONT_PATH),
+                font_size: 25.,
+                color: Color::BLACK,
+            },
+            TextAlignment {
+                horizontal: HorizontalAlign::Center,
+                ..default()
+            },
+        ),
+        ..default()
+    });
 }
